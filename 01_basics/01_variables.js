63c47f40c39d1b92ebf4199892b2a2eb966a2333
karangudha <karankgudha@gmail.com>
2025-01-07 10:11:52 +0530

    2nd commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 1234
+let accountEmail = "hi@gmail.com";
+var accountPassword = "12356"
+accountCity = "Jaipur"
+
+let accountState;
+
+//accountId = 2
+
+
+accountEmail = "hii@gmail.com"
+accountPassword = "222"
+accountCity = "sikar"
+
+/* 
+Prefer not to use var, beacause of issues with scope.
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use stricit"; //treat all JS code as newer version.
+
+//alert("hello") //we are using node.js, not browser,  so it will show error.
+
+//js autometicaly ignore spaces and it will automatically indendation the code.
+//documentation mdn web docs, tc39
+
+let name = "karan"
+let age = 18
+let isLoggedIn = false
+
+//range of number => 2 to power 53
+//string => you can use in single quotes and double quotes, but prefer double quotes
+//undefined => value is not defined
+//null => it is an empty value
+//symbol => it is unique value
+
+//object =>
+
+    //interview question 
+
+    console.log(typeof undefined ); => undefined
+    console.log(typeof null ); => object
+    console.log(typeof 10 ); => number
+    console.log(typeof "hello" ); => string
+
