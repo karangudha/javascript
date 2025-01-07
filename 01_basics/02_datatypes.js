17378f5651519eae39d5df0dd122650ddb91252f
karangudha <karankgudha@gmail.com>
2025-01-07 18:37:40 +0530

    4rd commit

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -24,3 +24,28 @@ let isLoggedIn = false
     // console.log(typeof 10 ); => number
     // console.log(typeof "hello" ); => string
 
+//Primitive types:
+
+// 7 types : string, Number, Boolean, null, undefined, Symbol, BigInt.
+
+// javascripit is staticlly typed.
+
+//Reference types (Non primitive)
+
+//Array , Object, Functions
+
+const heroes = ["ironman", "Caption", "Hulk"];
+let myObj = {
+    name: "karan",
+    age: 18,
+    isLoggedIn: false
+};
+
+const myFunction = function(){
+    console.log("Hello world!");
+};
+let BigInteger;
+console.log(typeof BigInteger);
+
+//The data type of all non primitive data type is function.
+//Data type of function is object function. 
